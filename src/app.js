51f9fb72bdabed183776b8f2b4bc36389517dd72
Erin Ringland <erinringland@gmail.com>
2020-12-17 16:44:36 -0600

    Displayed the results of the search

--- src/app.js
+++ src/app.js
@@ -1,24 +1,51 @@
 const searchForm = document.querySelector("form");
 const searchInput = document.querySelector("input");
 const searchResults = document.querySelector(".streets");
-const searchResultTitle = document.getElementById("street-name")
+const searchResultTitle = document.getElementById("street-name");
 const resultsTable = document.querySelector("tbody");
 
-function cleanPage(){
+function cleanPage() {
   searchResults.innerHTML = ``;
-  searchResultTitle.innerHTML = `Please enter a street name!`
+  searchResultTitle.innerHTML = `Please enter a street name!`;
   resultsTable.innerHTML = ``;
 }
+
+function getStreet(street) {
+  fetch(
+    `https://api.winnipegtransit.com/v3/streets.json?api-key=BGA5REIJoz3BbXP5CXN4&name=${street}&usage=long`
+  )
+    .then((response) => response.json())
+    .then((street) => {
+      street.streets.map((name) => {
+        searchResults.insertAdjacentHTML(
+          "beforeend",
+          `<a href="#">${name.name}</a>`
+        );
+        console.log(name.name);
+      });
+    });
+  // .then(street => console.log(street.streets));
+}
+
+getStreet();
+
+// function showStreet(street){
+//   street.map(name => {
+//     searchResults.insertAdjacentHTML('beforeend',
+//     `<a href="#">${name.name}</a>`);
+//     // console.log(name.name);
+//   })
+// }
 
 function search(e) {
   e.preventDefault();
   if (e.target.nodeName === "FORM") {
-    let searchTerm = searchInput.value;
-    console.log(searchTerm);
-    if (searchInput.value === "") {
-      searchResults.innerHTML = "<p>Please enter a street name!</p>";
-    }
+    getStreet(searchInput.value);
     searchInput.value = "";
+
+    // if (searchInput.value === "") {
+    //   searchResults.innerHTML = "<p>Please enter a street name!</p>";
+    // }
   }
 }
 
