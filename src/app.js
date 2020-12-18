ac009ef332aa036b7d52c90f8dbd2bd2edd05e99
Erin Ringland <erinringland@gmail.com>
2020-12-18 04:58:36 -0600

    Add the stops to the table and clean up the code

--- src/app.js
+++ src/app.js
@@ -34,39 +34,44 @@ function displaySearch(street) {
   });
 }
 
-function chosenStreet(streetID, streetName) {
+function chosenStreet(streetID) {
   fetch(
-    `https://api.winnipegtransit.com/v3/stops.json?api-key=BGA5REIJoz3BbXP5CXN4&street=${streetID}&usage=long`
+    `https://api.winnipegtransit.com/v3/stops.json?api-key=BGA5REIJoz3BbXP5CXN4&street=${streetID}`
   )
     .then((response) => response.json())
-    // .then((stops) => console.log(stops.stops));
     .then((stops) => {
-      // stops.stops.map((name) => console.log(name.key));
-      let schedule = stops.stops
-        .map((name) => name.key)
-        .map((route) => {
-          return fetch(
-            `https://api.winnipegtransit.com/v3/stops/${route}/schedule.json?api-key=BGA5REIJoz3BbXP5CXN4&max-results-per-route=2&usage=long`
-          ).then((response) => response.json());
-        });
-      Promise.all(schedule).then((schedule) => displaySchedule(schedule));
-      // Promise.all(schedule).then(schedule => console.log(schedule));
+      let info = stops.stops.map(function (name) {
+        return fetch(
+          `https://api.winnipegtransit.com/v3/stops/${name.key}/schedule.json?api-key=BGA5REIJoz3BbXP5CXN4&max-results-per-route=2`
+        ).then((response) => response.json());
+      });
+      Promise.all(info).then((info) => {
+        for (const items of info) {
+          for (const route of items["stop-schedule"]["route-schedules"]) {
+            displaySchedule(route, items["stop-schedule"].stop);
+          }
+        }
+      });
     });
 }
 
-function displaySchedule(times) {
-  resultsTable.innerHTML = times
-    .map(function (item) {
-      return `<tr>
-      <td>${item.stop - schedule.stop.street.name}</td>
-      <td>${item}</td>
-      <td>${item}</td>
-      <td>${item}</td>
-      <td>${item}</td>
-      </tr>
-      `;
-    })
-    .join("");
+function displaySchedule(route, info) {
+  searchResultTitle.innerHTML = `You are viewing the stops for ${info.street.name}`;
+
+  route["scheduled-stops"].forEach((row) => {
+    const date = new Date(row.times.arrival.estimated);
+    const newDate = date.toLocaleString().slice(12);
+    resultsTable.insertAdjacentHTML(
+      "beforeend",
+      `<tr>
+        <td>${info.street.name}</td>
+        <td>${info["cross-street"].name}</td>
+        <td>${info.direction}</td>
+        <td>${route.route.number}</td>
+        <td>${newDate}</td>
+        </tr>`
+    );
+  });
 }
 
 function search(e) {
@@ -86,9 +91,8 @@ searchForm.addEventListener("submit", search);
 
 searchResults.addEventListener("click", (e) => {
   if (e.target.nodeName === "A") {
-    let streetName = e.target.innerHTML;
     let streetID = e.target.dataset.id;
-    chosenStreet(streetID, streetName);
+    chosenStreet(streetID);
   }
 });
 
