0bdfc327bc434c581a6b49ecbb8da78b2d9e6c85
Erin Ringland <erinringland@gmail.com>
2020-12-18 01:33:23 -0600

    Added the stop schedule fetch

--- src/app.js
+++ src/app.js
@@ -34,6 +34,41 @@ function displaySearch(street) {
   });
 }
 
+function chosenStreet(streetID, streetName) {
+  fetch(
+    `https://api.winnipegtransit.com/v3/stops.json?api-key=BGA5REIJoz3BbXP5CXN4&street=${streetID}&usage=long`
+  )
+    .then((response) => response.json())
+    // .then((stops) => console.log(stops.stops));
+    .then((stops) => {
+      // stops.stops.map((name) => console.log(name.key));
+      let schedule = stops.stops
+        .map((name) => name.key)
+        .map((route) => {
+          return fetch(
+            `https://api.winnipegtransit.com/v3/stops/${route}/schedule.json?api-key=BGA5REIJoz3BbXP5CXN4&max-results-per-route=2&usage=long`
+          ).then((response) => response.json());
+        });
+      Promise.all(schedule).then((schedule) => displaySchedule(schedule));
+      // Promise.all(schedule).then(schedule => console.log(schedule));
+    });
+}
+
+function displaySchedule(times) {
+  resultsTable.innerHTML = times
+    .map(function (item) {
+      return `<tr>
+      <td>${item.stop - schedule.stop.street.name}</td>
+      <td>${item}</td>
+      <td>${item}</td>
+      <td>${item}</td>
+      <td>${item}</td>
+      </tr>
+      `;
+    })
+    .join("");
+}
+
 function search(e) {
   e.preventDefault();
   if (e.target.nodeName === "FORM") {
@@ -49,12 +84,12 @@ function search(e) {
 
 searchForm.addEventListener("submit", search);
 
-searchResults.addEventListener("click",  e => {
-  if(e.target.nodeName === "A"){
+searchResults.addEventListener("click", (e) => {
+  if (e.target.nodeName === "A") {
     let streetName = e.target.innerHTML;
     let streetID = e.target.dataset.id;
-    console.log(streetID, streetName)
+    chosenStreet(streetID, streetName);
   }
-})
+});
 
 cleanPage();
